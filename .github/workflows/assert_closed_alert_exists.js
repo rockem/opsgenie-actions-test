b658825e9fb03f64380c1c3327b7571f2d5d4619
Eli Segal <eli.segal@gmail.com>
2022-01-31 15:59:07 +0200

    Fixed validation script

--- .github/workflows/assert_closed_alert_exists.js
+++ .github/workflows/assert_closed_alert_exists.js
@@ -1,13 +1,10 @@
 const https = require('https');
 
-
 const alias = process.argv[2];
-console.log(`alias: ${alias}`)
-
 
 const options = {
     hostname: 'api.opsgenie.com',
-    path: `/v2/alerts/${alias}?identifierType=alias`,
+    path: `/v2/alerts?query=${(encodeURIComponent(`alias:${alias} AND status:closed`))}`,
     headers: {
         Authorization: `GenieKey ${process.env.OPSGENIE_API_KEY}`
     }
@@ -23,15 +20,15 @@ https.get(options, (resp) => {
     resp.on('end', () => {
         if(resp.statusCode !== 200) {
             console.log(`ERROR: ${data}`)
-            process.exit(1)
+            process.exit(2)
         }
-        if(JSON.parse(data)['data']['status'] !== 'closed') {
-            console.log(`Alert with alias: "${alias}" isn't closed`)
+        if(JSON.parse(data).length === 0) {
+            console.log(`Failed to find closed alert with alias [${alias}]`)
             process.exit(2)
         }
     });
 
 }).on("error", (err) => {
-    console.log("Error: " + err.message);
+    console.log("ERROR: " + err.message);
     process.exit(1);
 });
